41f48c610d670f58c464e3c692aef28c7781dee4
mattila0611 <mattila0611@gmail.com>
2023-08-17 18:14:18 +0200

    implement quiz fetching on quiz list page

--- frontend/src/components/QuizListElement.tsx
+++ frontend/src/components/QuizListElement.tsx
@@ -2,17 +2,18 @@ import {QuizDetails} from "../pages/QuizList";
 import {ReactElement} from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import {Card, CardActions, CardContent} from "@mui/material";
+import {Card, CardActions, CardContent, getTableSortLabelUtilityClass} from "@mui/material";
 import Button from "@mui/material/Button";
 import WeightIcon from '@mui/icons-material/FitnessCenter';
 
-export default function QuizListElement({name, author, questionQuantity, difficulty}: QuizDetails): ReactElement {
+export default function QuizListElement({name, author, numberOfQuestions, difficulty}: QuizDetails): ReactElement {
+    console.log(difficulty)
     return (
         <Grid item xs={12} sm={6} md={3}>
             <Card variant={"outlined"} sx={{display: "flex", alignItems: "center", justifyContent: "space-between", flexDirection: {xs: "column", md: "row"}}}>
                 <CardContent sx={{display: "flex", alignItems: "center", width: "80%", justifyContent: "space-between", flexDirection: {xs: "column", md: "row"}}}>
                     <Typography variant={"h6"} fontWeight={"bold"}>{name}</Typography>
-                    <Typography>Questions: {questionQuantity}</Typography>
+                    <Typography>Questions: {numberOfQuestions}</Typography>
                     <WeightIcon htmlColor={difficulty}/>
 
                     <Typography>Author: {author}</Typography>
--- frontend/src/pages/QuizList.tsx
+++ frontend/src/pages/QuizList.tsx
@@ -1,52 +1,60 @@
-import {ReactElement} from "react";
+import {ReactElement, useEffect, useState} from "react";
 import Container from "@mui/material/Container";
 import QuizListElement from "../components/QuizListElement";
 import Grid from "@mui/material/Grid";
 import {Stack} from "@mui/material";
 import Typography from "@mui/material/Typography";
+import {Question} from "../components/QuestionListElement";
 
-export enum QuizDifficulty {
-    EASY = "#2b9f00",
-    MEDIUM = "#f5c400",
-    HARD = "#c90d00"
+export const QuizDifficulty = {
+    EASY: "#2b9f00",
+    MEDIUM: "#f5c400",
+    HARD: "#c90d00"
 }
+
+type Difficulty = "EASY" | "MEDIUM" | "HARD";
 
 export interface QuizDetails {
     name: string,
-    questionQuantity: number,
+    numberOfQuestions: number,
     difficulty: string,
     author: string
 }
 
-const testDetails: QuizDetails[] = [
-    {name: "test1", questionQuantity: 3, difficulty: QuizDifficulty.HARD, author: "testuser"},
-    {name: "test2", questionQuantity: 7, difficulty: QuizDifficulty.MEDIUM, author: "testuser"},
-    {name: "test3", questionQuantity: 31, difficulty: QuizDifficulty.HARD, author: "testuser"},
-    {name: "test4", questionQuantity: 45, difficulty: QuizDifficulty.HARD, author: "testuser"},
-    {name: "test5", questionQuantity: 3, difficulty: QuizDifficulty.EASY, author: "testuser"},
-    {name: "test6", questionQuantity: 7, difficulty: QuizDifficulty.EASY, author: "testuser"},
-    {name: "test7", questionQuantity: 0, difficulty: QuizDifficulty.HARD, author: "testuser"},
-    {name: "test8", questionQuantity: 0, difficulty: QuizDifficulty.HARD, author: "testuser"},
-];
-
 function QuizList(): ReactElement{
-    return(
-            <Container maxWidth={"lg"}>
-                <Typography variant={"h4"} mb={4}>
-                    Available quizzes:
-                </Typography>
-                <Stack>
-                    {testDetails.map((testDetail: QuizDetails, i: number) => (
-                        <QuizListElement key={i}
-                                         name={testDetail.name}
-                                         questionQuantity={testDetail.questionQuantity}
-                                         difficulty={testDetail.difficulty}
-                                         author={testDetail.author}
-                        />
-                    ))}
-                </Stack>
-            </Container>
-        );
+    const [quizzes, setQuizzes] = useState<QuizDetails[] | null>(null);
+
+    const fetchQuizzes = async () => {
+        const res = await fetch("/api/quiz/all/details");
+        const data = await res.json();
+
+        console.log(data)
+        setQuizzes(data);
+    }
+
+    useEffect(() => {
+        fetchQuizzes();
+    },[]);
+
+    if(quizzes){
+        return(
+                <Container maxWidth={"lg"}>
+                    <Typography variant={"h4"} mb={4}>
+                        Available quizzes:
+                    </Typography>
+                    <Stack>
+                        {quizzes.map((quiz: QuizDetails, i: number) => (
+                            <QuizListElement key={i}
+                                             name={quiz.name}
+                                             numberOfQuestions={quiz.numberOfQuestions}
+                                             difficulty={QuizDifficulty[quiz.difficulty as Difficulty]}
+                                             author={quiz.author}
+                            />
+                        ))}
+                    </Stack>
+                </Container>
+            );
+    } else return (<></>)
 }
 
 export default QuizList;
