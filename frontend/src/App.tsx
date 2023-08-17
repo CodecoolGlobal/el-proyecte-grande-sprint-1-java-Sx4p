d405a89d7468c53eb4d0c16b8c7d5b9354585513
mattila0611 <mattila0611@gmail.com>
2023-08-17 11:50:58 +0200

    change page styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,22 @@
-import React, { createContext} from 'react';
+import React from 'react';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import Login from "./pages/Login";
 import {Theme} from "@emotion/react";
-import {createTheme} from "@mui/material/styles";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import QuizList from "./pages/QuizList";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout/>,
     children: [
+        {
+            path: "/list",
+            element: <QuizList/>
+        }
     ]
   },
   {
@@ -21,29 +27,42 @@ const router = createBrowserRouter([
 
 const theme: Theme = createTheme(
     {
-      palette: {
-        mode: 'light',
-        primary: {
-          main: '#128792',
+        palette: {
+            primary: {
+                main: '#2196f3',
+                light: '#3ec8fe',
+                dark: '#1d6fb1',
+            },
+            secondary: {
+                main: '#d50000',
+            },
+            background: {
+                default: '#e1f5fe',
+                paper: 'rgba(255,255,255,0.9)',
+            },
+            text: {
+                primary: '#000000',
+            },
         },
-        secondary: {
-          main: '#f50000',
+        typography: {
+            fontFamily: 'Rubik',
+            fontSize: 15,
+            fontWeightLight: 200,
+            fontWeightBold: 700,
+            button: {
+                fontFamily: 'PT Sans',
+            },
         },
-      },
-      typography: {
-        fontFamily: 'Droid Serif',
-      },
     }
 );
-export const ThemeContext = createContext(theme);
-
 function App() {
   return (
-      <ThemeContext.Provider value={theme}>
-        <div>
-          <RouterProvider router={router}/>
-        </div>
-      </ThemeContext.Provider>
+      <>
+          <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <RouterProvider router={router}/>
+          </ThemeProvider>
+      </>
   );
 }
 
