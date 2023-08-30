53ebe45e842a4643ead645e96088d069cb60a604
erdelyieszter <esztergyerdelyi@gmail.com>
2023-08-30 12:24:52 +0200

    create Entities from classes

--- backend/src/main/java/com/example/backend/model/Question.java
+++ backend/src/main/java/com/example/backend/model/Question.java
@@ -1,6 +1,31 @@
 package com.example.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.Set;
 
-public record Question(int id, String question, Set<Answer> answers) {
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String question;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    private Set<Answer> answers;
+
+    @JsonBackReference
+    @ManyToOne
+    private Quiz quiz;
 }
